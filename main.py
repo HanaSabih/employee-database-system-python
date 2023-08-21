829d53b5e1c5058143cfc2816a5b49056bc6fdd1
hana <hanasabih@gmail.com>
2023-08-21 16:00:59 +0300

    i build a function male_female_statistics that count the numbers of male and female emplyees with o(n) where n is the number of employees

--- main.py
+++ main.py
@@ -21,59 +21,76 @@ class Empolyees_Data():
             employee_id, username, timestamp, gender, salary = x.split(', ')
             self.employees[employee_id] = Single_Employee(employee_id, username, timestamp, gender, int(salary))
             
+    # o(n) =>n is the numbers of employees
+    def male_female_statistics(self):
+        count_male =0
+        count_female = 0
+        for employee in self.employees.values():
+            if employee.gender == "female":
+                count_female +=1 
+            elif employee.gender == "male":
+                count_male +=1
+        print(f'there is: {count_male} Male Employees, and {count_female} Female Employees')
+
+
+
+Empolyees_Data().male_female_statistics()   
+# output ====>> there is: 1 Male Employees, and 1 Female Employees
+ 
 
 
 
 
 
-def display_menu():
-    all_data = Empolyees_Data()
-    print("\nWelcome to the Employee Database System")
 
-    for i in range (5):       
-        user_name = input("Enter username:") 
-        user_password = input("Enter password:")
+# def display_menu():
+#     all_data = Empolyees_Data()
+#     print("\nWelcome to the Employee Database System")
+
+#     for i in range (5):       
+#         user_name = input("Enter username:") 
+#         user_password = input("Enter password:")
    
-        if user_name == "admin" and user_password == "admin123123":
-            while True:
-                print("\nThe admin menu:")
-                print("1 -> Display female and male statistics")
-                print("2 -> Add a single employee")
-                print("3 -> Display all the employees")
-                print("4 -> Change a specific employee Salary")
-                print("5 -> Remove a specific employee")
-                print("6 -> Raise a specific employee's Salary")
-                print("7 -> Exit")
+#         if user_name == "admin" and user_password == "admin123123":
+#             while True:
+#                 print("\nThe admin menu:")
+#                 print("1 -> Display female and male statistics")
+#                 print("2 -> Add a single employee")
+#                 print("3 -> Display all the employees")
+#                 print("4 -> Change a specific employee Salary")
+#                 print("5 -> Remove a specific employee")
+#                 print("6 -> Raise a specific employee's Salary")
+#                 print("7 -> Exit")
 
-                choice_number = input("Please enter your choice: ")
+#                 choice_number = input("Please enter your choice: ")
 
-                if choice_number == "1":
-                    print("you choose 1")
-                elif choice_number == "2":
-                    print("you choose 2")
-                elif choice_number == "3":
-                    print("you choose 3")
-                elif choice_number == "4":
-                    print("you choose 4")
-                elif choice_number == "5":
-                    print("you choose 5")
-                elif choice_number == "6":
-                    print("you choose 6")
-                elif choice_number == "7":
-                    print("you choose 7")
-                    break
-                else:
-                    print("invalid number, please try again")
+#                 if choice_number == "1":
+#                     print("you choose 1")
+#                 elif choice_number == "2":
+#                     print("you choose 2")
+#                 elif choice_number == "3":
+#                     print("you choose 3")
+#                 elif choice_number == "4":
+#                     print("you choose 4")
+#                 elif choice_number == "5":
+#                     print("you choose 5")
+#                 elif choice_number == "6":
+#                     print("you choose 6")
+#                 elif choice_number == "7":
+#                     print("you choose 7")
+#                     break
+#                 else:
+#                     print("invalid number, please try again")
 
-        elif user_name == "manuella":
-            print("it is normal user")
-            break
+#         elif user_name == "manuella":
+#             print("it is normal user")
+#             break
    
     
-        elif i< 4:
-            print(f"Incorrect Username and/or Password. you still have {4-i} trial")
-        else:
-            print("Sorry you are blocked ,you have reached all the trials")
+#         elif i< 4:
+#             print(f"Incorrect Username and/or Password. you still have {4-i} trial")
+#         else:
+#             print("Sorry you are blocked ,you have reached all the trials")
 
-display_menu()
+# display_menu()
 
