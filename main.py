c9c477def1cc6e6675fe95718ce07fade7308888
hana <hanasabih@gmail.com>
2023-08-21 10:50:32 +0300

    i implemented display_menu() where the function will loop 5 times if the username and password are incorrect else if it is admin it will write it is admin or if it is normal user it will print it is a normal user

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ class Single_Employee:
         self.salary = salary
 
 
-
+# o(n) => n =the number of lines
 def read_employees_data_file():
     f = open("employees_data.txt", "r")
     employees = {}
@@ -19,3 +19,27 @@ def read_employees_data_file():
   
 read_employees_data_file()
 
+
+def display_menu():
+    read_employees_data_file()
+    for i in range (5):
+        print("\nWelcome to the Employee Database System")
+        user_name = input("Enter username:") 
+        user_password = input("Enter password:")
+
+    
+        if user_name == "admin" and user_password == "admin123123":
+
+            print("it is admin")
+            break
+        elif user_name == "manuella":
+            print("it is normal user")
+            break
+   
+    
+        if i< 4:
+            print(f"Incorrect Username and/or Password. you still have {4-i} trial")
+        else:
+            print("Sorry you are blocked ,you have reached all the trials")
+
+display_menu()
